ba21b8768f2aa3729d1258e77be8a9ca6544bf85
MohsenMokhtari <m.mokhtari2012@gmail.com>
2017-05-17 23:00:33 +0300

    simple time for traffic light

---
+++ traffic.java
@@ -0,0 +1,14 @@
+public class traffic{
+  public static void main(String[] args){
+
+     double light=5;
+
+     if(light==5){
+      for(int i=0;i<5;i++){
+        System.out.println("Green");
+      }
+    }else{
+        System.out.println("ERROR");
+    }
+  }
+}
